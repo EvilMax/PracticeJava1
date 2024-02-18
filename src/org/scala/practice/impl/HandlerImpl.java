9580d833788a426a9ba4c11484b4fad826141bf9
max <evilmax@gmail.com>
2024-02-18 20:02:01 +0300

    Removed unused imports

--- src/org/scala/practice/impl/HandlerImpl.java
+++ src/org/scala/practice/impl/HandlerImpl.java
@@ -15,8 +15,6 @@ import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicInteger;
